361d5fac69dd4dffff357ecf608b05d522c4f9ae
FranJaviMN <franjaviermn17100@gmail.com>
2022-09-26 17:48:06 +0200

    Agregado un "Hello world" como prueba en C

---
+++ Programación/Lenguaje C/hello_world.c
@@ -0,0 +1,10 @@
+//Incluimos libreria de entrada/salida, de las mas importantes.
+# include <stdio.h>
+
+
+//Declaramos una función que nos va a dar como resultado un mensaje
+int main() {
+    //Usamos printf para poder mostrar por pantalla un mensaje.
+    printf("Hola Mundo!!\n");
+    return 0;
+}
