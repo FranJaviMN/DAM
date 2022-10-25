1966b2e990e848ab611e85065aa3ce9cb804b5df
FranJaviMN <franjaviermn17100@gmail.com>
2022-10-25 20:24:04 +0200

    Agregado comienzo del ejercicio 8

---
+++ Programación/Lenguaje C/Boletin 9 - Programación modular/ejercicio8/Ejercicio 8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+
+
+void main(){
+
+    float distancia_vuelo;
+    int dia_ida;
+    int mes_ida;
+    int dia_vuelta;
+    int mes_vuelta;
+
+    printf("Dime la distancia que hay de vuelo: ");
+    scanf("\n%f",&distancia_vuelo);
+    fflush(stdin);
+
+    printf("Dime el dia de ida: ");
+    scanf("\n%i",&dia_ida);
+    fflush(stdin);
+
+    printf("Dime el dia de ida: ");
+    scanf("\n%i",&dia_ida);
+    fflush(stdin);
+
+
+}
